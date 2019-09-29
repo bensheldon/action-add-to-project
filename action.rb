ac821780bd52d946876cc539d4baf21525b4aa9a
Ben Sheldon <bensheldon@gmail.com>
2019-09-29 13:33:07 -0700

    Use new action syntax

--- action.rb
+++ action.rb
@@ -1,37 +1,34 @@
-require 'bundler/inline'
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
 
-gemfile do
-  source 'https://rubygems.org'
-
-  gem "json", "~> 2.2.0"
-  gem "octokit", "~> 4.14.0"
-end
-
+GITHUB_EVENT_PATH = ENV.fetch('GITHUB_EVENT_PATH')
 GITHUB_TOKEN = ENV.fetch('GITHUB_TOKEN')
-COLUMN_ID = ENV.fetch('COLUMN_ID')
+COLUMN_ID = ENV.fetch('INPUT_COLUMN_ID')
 
-PULL_REQUEST_VALID_ACTIONS = ['opened']
-ISSUE_VALID_ACTIONS = ['opened']
-
-event_json = File.read(ENV['GITHUB_EVENT_PATH'])
+event_json = File.read(GITHUB_EVENT_PATH)
 event = JSON.parse(event_json)
 
 client = Octokit::Client.new(access_token: GITHUB_TOKEN)
-# current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
 
+# puts event_json
+# puts ENV.reject { |k, v| k == 'GITHUB_TOKEN' }
+# current_repo = Octokit::Repository.from_url(event["repository"]["html_url"])
 # projects = client.projects(current_repo, accept: "application/vnd.github.inertia-preview+json")
 # puts projects.inspect
 
-if event['pull_request'] && PULL_REQUEST_VALID_ACTIONS.include?(event['action'])
+if event['pull_request']
   content_id = event['pull_request']['id']
   content_type = 'PullRequest'
 end
 
-if event['issue'] && ISSUE_VALID_ACTIONS.include?(event['action'])
+if event['issue']
   content_id = event['issue']['id']
   content_type = 'Issue'
 end
 
 if content_id && content_type
   client.create_project_card(COLUMN_ID, content_id: content_id, content_type: content_type, accept: "application/vnd.github.inertia-preview+json")
+else
+  puts "Skipping because trigger not a Pull Request or Issue"
 end
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 set -e
-sh -c "ruby /action.rb $*"
+sh -c "bundle exec ruby /action.rb $*"
